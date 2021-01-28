fd72dc6920be48a109a6414c8c5754c8eac563f8
tihero <tihero@yandex.ru>
2021-01-28 17:50:39 +0300

    homework-2

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from '../css/footer.module.css';
+
+const Footer = () => {
+    return(
+        <footer>
+            <div className={s.wrapper}>
+                <h3>THANKS FOR VISITING</h3>
+                <p>© 2021 #ReactMarathon.</p>
+            </div>
+        </footer>
+    )
+};
+export default Footer;
